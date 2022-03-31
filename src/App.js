8ed0c2be043c6351c3576783e825e8573e67bac2
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-03-31 15:54:33 +0100

    changed state to object instead of array and rearranged evertything

--- src/App.js
+++ src/App.js
@@ -1,87 +1,39 @@
 import "./App.css";
 import Die from "./components/Die";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export default function App() {
+  const [dice, setDice] = useState([
+    {
+      isHeld: false,
+      value: newDice(),
+    },
+  ]);
+
+  function newDice() {
+    let allValues = [];
+    for (let i = 1; i <= 10; i++) {
+      const oneToSixNumber = Math.floor(Math.random() * 6) + 1;
+      allValues.push(oneToSixNumber);
+    }
+    return allValues;
+  }
+
+  function rollingDice() {
+    setDice((prev) => prev.map((die) => ({ ...die, value: newDice() })));
+  }
+
+  //useEffect(newDice, []);
+
+  const die = dice[0].value.map((die, ind) => <Die key={ind} value={die} />);
 
   return (
     <main className="mainBody">
       <h1 className="mainTitle">Tenzies app</h1>
-      <div className="boxesContainer">
-        {boxes.map((box) => (
-          <Die value={box} />
-        ))}
-      </div>
+      <div className="boxesContainer">{die}</div>
+      <button onClick={rollingDice} className="rollDice">
+        Roll me
+      </button>
     </main>
   );
 }
-
-export default App;
-
-// export default function Sidebar(props) {
-
-//     const noteElements = props.notes.map((note, index) =>  {
-
-//        let firstLine = note.body.split("\n");
-//        console.log(firstLine);
-
-//       return  (
-//       <div key={note.id}>
-//             <div
-//                 className={`title ${
-//                     note.id === props.currentNote.id ? "selected-note" : ""
-//                 }`}
-//                 onClick={() => props.setCurrentNoteId(note.id)}
-//             >
-//                 <h4 className="text-snippet">Note {index + 1}</h4>
-//             </div>
-//         </div>
-//     )
-//     }
-
-//     return (
-//         <section className="pane sidebar">
-//             <div className="sidebar--header">
-//                 <h3>Notes</h3>
-//                 <button className="new-note" onClick={props.newNote}>+</button>
-//             </div>
-//             {noteElements}
-//         </section>
-//     )
-// }
-
-// function updateNote(text) {
-
-//   let newArr = [];
-//   for(let i=0; i < notes.length; i++){
-//       if(notes[i].id === currentNoteId){
-//           notes[i].body = text;
-//           newArr.unshift(notes[i]);
-//       }else{
-//           newArr.push(notes[i])
-//       }
-//   }
-
-//    setNotes(newArr);
-
-// }
-
-// function findCurrentNote() {
-//   return notes.find(note => {
-//       return note.id === currentNoteId
-//   }) || notes[0]
-// }
-
-// function deleteNote(event, noteId) {
-//   event.stopPropagation();
-//   console.log(noteId);
-
-//   setNotes(prev => {
-//       return prev.filter(el => el.id !== noteId)
-//   })
-// }
-
-// {/* <button
-//                     className="delete-btn"
-//                     onClick={(e)=> props.deleteNote(e, note.id)}// Your onClick event handler here
-//                 ></button> */}
